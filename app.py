c2427c72bc315037b3abe0d00d668bd60263509b
Tos0301 <tos-141sousche@outlook.jp>
2025-05-13 13:34:47 +0900

    Fix port binding and add index page

--- app.py
+++ app.py
@@ -85,4 +85,5 @@ def checkout():
     return render_template('thanks.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 1000))
+    app.run(host='0.0.0.0', port=port)
